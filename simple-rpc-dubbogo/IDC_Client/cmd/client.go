fa6793b801cc0a94e630338b7ad68b9f86b536f5
小熊 <627516430@qq.com>
2023-09-15 15:29:54 +0800

    提交：优化将环境变量配置写在代码中替代手动在终端中输入【完成一次直接定义的接口RPC调用（使用dubbp-go框架）】

--- simple-rpc-dubbogo/IDC_Client/cmd/client.go
+++ simple-rpc-dubbogo/IDC_Client/cmd/client.go
@@ -3,6 +3,8 @@ package main
 import (
 	"context"
 	"dubbo-go-client/api"
+	"flag"
+	"os"
 
 	"dubbo.apache.org/dubbo-go/v3/common/logger"
 	"dubbo.apache.org/dubbo-go/v3/config"
@@ -12,6 +14,20 @@ import (
 var grpcGenerator1Impl = new(api.Generator1ClientImpl)
 
 var grpcXTestImpl = new(api.XtestClientImpl) // 确保在使用之前进行初始化
+
+func init() {
+	// 使用命令行参数来指定配置文件路径
+	configFile := flag.String("config", "./conf/dubbogo.yaml", "Path to Dubbo-go config file")
+	flag.Parse()
+
+	// 设置 DUBBO_GO_CONFIG_PATH 环境变量
+	os.Setenv("DUBBO_GO_CONFIG_PATH", *configFile)
+
+	// 加载 Dubbo-go 的配置文件，根据环境变量 DUBBO_GO_CONFIG_PATH 中指定的配置文件路径加载配置信息。配置文件通常包括 Dubbo 服务的注册中心地址、协议、端口等信息。
+	if err := config.Load(); err != nil {
+		panic(err)
+	}
+}
 
 func main() {
 	config.SetConsumerService(grpcGenerator1Impl)
--- simple-rpc-dubbogo/IDC_Service/cmd/app.go
+++ simple-rpc-dubbogo/IDC_Service/cmd/app.go
@@ -2,10 +2,26 @@ package main
 
 import (
 	_ "dubbo-go-app/pkg/service"
+	"flag"
+	"os"
 
 	"dubbo.apache.org/dubbo-go/v3/config"
 	_ "dubbo.apache.org/dubbo-go/v3/imports"
 )
+
+func init() {
+	// 使用命令行参数来指定配置文件路径
+	configFile := flag.String("config", "conf/dubbogo.yaml", "Path to Dubbo-go config file")
+	flag.Parse()
+
+	// 设置 DUBBO_GO_CONFIG_PATH 环境变量
+	os.Setenv("DUBBO_GO_CONFIG_PATH", *configFile)
+
+	// 加载 Dubbo-go 的配置文件，根据环境变量 DUBBO_GO_CONFIG_PATH 中指定的配置文件路径加载配置信息。配置文件通常包括 Dubbo 服务的注册中心地址、协议、端口等信息。
+	if err := config.Load(); err != nil {
+		panic(err)
+	}
+}
 
 // export DUBBO_GO_CONFIG_PATH=$PATH_TO_APP/conf/dubbogo.yaml
 func main() {
