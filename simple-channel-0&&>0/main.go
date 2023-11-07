d4d9af6a49c89c5bfd53fa9e3cb57b2c50d3bb53
小熊 <627516430@qq.com>
2023-11-07 18:05:20 +0800

    将管道应用于一个生产者-消费者模式当中，通过管道实现多个协程的协调与同步

--- simple-channel-0&&>0/main.go
+++ simple-channel-0&&>0/main.go
@@ -1,11 +1,128 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+/**
+管道-通道-Channel
+0. 基本概念：管道底层是一个【环形队列】，先进先出。特别适合多协程并发的场景。
+	* 一旦在多协程情况下，需要多个协程共同去读写某个容器，这种情况应该先想到管道。
+	* make(chan int, 8)		8 代表容量，不是长度，即刚创建里面还是没有元素的。
+1. 将管道应用于一个生产者-消费者模式当中
+2. 管道的阻塞问题
+	* sync.WaitGroup{}
+3. 如何通过管道实现多个协程的协调与同步
+	* make(chan struct{}, 0)
+*/
+
+// 1. 将管道应用于一个生产者-消费者模式当中
+func ProducerConsumer() {
+	ch := make(chan int, 100)
+	wg := sync.WaitGroup{}
+	wg.Add(2)
+	// 2个生产者,往channel内写元素
+	go func() {
+		defer wg.Done()
+		for i := 0; i < 10; i++ {
+			ch <- i
+		}
+	}()
+	go func() {
+		defer wg.Done()
+		for i := 0; i < 10; i++ {
+			ch <- i
+		}
+	}()
+
+	// wg2 := sync.WaitGroup{}
+	// wg2.Add(1)
+	mc := make(chan struct{}, 0) //空结构体，不占任何内存空间，并且增强可读性（该管道不是用做容器使用，而是充当用于一个协程之间的协调同步作用）
+	// 1个消费者
+	go func() {
+		// defer wg2.Done()
+		defer func() {
+			mc <- struct{}{}
+		}()
+		sum := 0
+		for {
+			v, ok := <-ch
+			if !ok { //channel被关闭，且channel为空，此时OK才为false
+				break
+			}
+			sum += v
+		}
+		fmt.Println("sum=", sum)
+	}()
+
+	wg.Wait()
+	close(ch) //意味着，从此之后管道不可用了，不允许再往管道里面写入元素了，但仍然可以读取
+	// wg2.Wait()
+	<-mc
+}
 
 func main() {
-	ch1 := make(chan int, 1) //有缓存通道，可以写
-	ch1 <- 1                 //可以写
-	fmt.Println("ch1 有缓存通道，可以直接写")
+	ProducerConsumer()
+
+	ch := make(chan int, 0) //容量为0的管道
+	fmt.Println(time.Now().Unix())
+	go func() {
+		ch <- 4
+		fmt.Println(time.Now().Unix(), "写入4到管道成功")
+	}()
+	time.Sleep(2 * time.Second)
+	go func() {
+		v := <-ch
+		fmt.Println(time.Now().Unix(), "读取管道成功, v=", v)
+	}()
+	time.Sleep(1 * time.Second)
+	/** 上面代码的输出结果：
+	1699344916
+	1699344918 写入4到管道成功
+	1699344918 读取管道成功, v= 4
+	*/
+
+	ch02 := make(chan int, 1) //容量为1的管道
+	fmt.Println(time.Now().Unix())
+	go func() {
+		ch02 <- 5
+		fmt.Println(time.Now().Unix(), "写入5到管道成功")
+	}()
+	time.Sleep(2 * time.Second)
+	go func() {
+		v := <-ch02
+		fmt.Println(time.Now().Unix(), "读取管道成功, v=", v)
+	}()
+	time.Sleep(1 * time.Second)
+	/** 上面代码的输出结果：
+	1699345176
+	1699345176 写入5到管道成功
+	1699345178 读取管道成功, v= 5
+	*/
+
+	ch03 := make(chan int, 1)
+	fmt.Println(time.Now().Unix())
+	go func() {
+		v := <-ch03
+		fmt.Println(time.Now().Unix(), "读取管道成功, v=", v)
+	}()
+	time.Sleep(2 * time.Second)
+	go func() {
+		ch03 <- 6
+		fmt.Println(time.Now().Unix(), "写入6到管道成功")
+	}()
+	time.Sleep(1 * time.Second)
+	/** 上面代码的输出结果：
+	1699345458
+	1699345460 写入6到管道成功
+	1699345460 读取管道成功, v= 6
+	*/
+
+	// ch1 := make(chan int, 1) //有缓存通道，可以写
+	// ch1 <- 1                 //可以写
+	// fmt.Println("ch1 有缓存通道，可以直接写")
 
 	// ch2 := make(chan int) //无缓存通道
 	// ch2 <- 1              //fatal error: all goroutines are asleep - deadlock!
